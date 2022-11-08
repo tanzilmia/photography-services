0560fe2b8fb1aa46dde7dce0acbaad89d06749c6
tanzilmia <ziltan590@gmail.com>
2022-11-08 21:34:24 +0600

    image full screen view

--- src/SubComponent/ServiceCard/ServiceCard.js
+++ src/SubComponent/ServiceCard/ServiceCard.js
@@ -2,31 +2,41 @@ import React from "react";
 import { AiTwotoneStar } from 'react-icons/ai';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 const ServiceCard = ({ service }) => {
   console.log(service);
   const { description, image, price, rating, service_name, _id } = service;
   let shorDescirption = description.slice(0,100)
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure className="h-80">
-          <img src={image} className="h-full w-11/12 rounded-xl" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <div className="flex justify-between">
-            {" "}
-            <h2 className="card-title text-2xl text-purple-900 font-bold">{service_name}</h2>
-            <h2 className="text-2xl font-bold text-yellow-500"> ${price} </h2>
-          </div>
-          <p> {shorDescirption}... </p>
-          <div className="card-actions flex justify-between items-center">  
-            <Link to = {`/services/${_id}`} className="btn btn-outline btn-success">View Details <AiOutlineArrowRight className="ml-3"/> </Link>
-            <div className="flex items-center text-lime-400"> <AiTwotoneStar className="mr-1" /> {rating} </div>
-          </div>
-        </div>
-      </div>
+
+    <PhotoProvider>
+     <PhotoView src= {image}>
+     <div>
+    <div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="h-80">
+    <img src={image} className="h-full w-11/12 rounded-xl" alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <div className="flex justify-between">
+      {" "}
+      <h2 className="card-title text-2xl text-purple-900 font-bold">{service_name}</h2>
+      <h2 className="text-2xl font-bold text-yellow-500"> ${price} </h2>
     </div>
+    <p> {shorDescirption}... </p>
+    <div className="card-actions flex justify-between items-center">  
+      <Link to = {`/services/${_id}`} className="btn btn-outline btn-success">View Details <AiOutlineArrowRight className="ml-3"/> </Link>
+      <div className="flex items-center text-lime-400"> <AiTwotoneStar className="mr-1" /> {rating} </div>
+    </div>
+  </div>
+</div>
+</div>
+    </PhotoView>
+    </PhotoProvider>
+
+    
   );
 };
 
 export default ServiceCard;
+
