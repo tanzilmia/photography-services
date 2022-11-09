6ac53dfdfd57bdfd71d405adc5a2ababcc2e928e
tanzilmia <ziltan590@gmail.com>
2022-11-09 09:18:54 +0600

    service details

--- src/DynamicPage/ServiceDetails/ServiceDetails.js
+++ src/DynamicPage/ServiceDetails/ServiceDetails.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { AiTwotoneStar } from "react-icons/ai";
 const ServiceDetails = () => {
-    const service = useLoaderData()
-    const { description, image, price, rating, service_name, _id } = service;
-    console.log(service)
-    return (
-        <div>
-            <h2 className='text-2xl'> Service Details Page {service_name}</h2>
+  const service = useLoaderData();
+  const { description, image, price, rating, service_name, _id } = service;
+  console.log(service);
+  return (
+    <div className="w-10/12 m-auto">
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img className="rounded-lg" src={image} alt="" />
+          <div>
+            <h1 className="text-5xl font-bold"> {service_name} </h1>
+            <p className="py-6 text-xl">{description}</p>
+            <div className="flex">
+              <h1 className="text-5xl font-bold text-sky-700"> $ {price} </h1>
+              <h1 className="text-5xl font-bold ml-20 flex text-amber-400">
+                {" "}
+                <AiTwotoneStar /> {rating}{" "}
+              </h1>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+
+      {/* review section */}
+    </div>
+  );
 };
 
 export default ServiceDetails;
