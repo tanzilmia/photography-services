19220421e90af3bda9b532ec0b3df10dd6233827
tanzilmia <ziltan590@gmail.com>
2022-11-09 09:49:07 +0600

    review section form design

--- src/DynamicPage/ServiceDetails/ServiceDetails.js
+++ src/DynamicPage/ServiceDetails/ServiceDetails.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import { AiTwotoneStar } from "react-icons/ai";
+import './Servicedetails.css'
 const ServiceDetails = () => {
   const service = useLoaderData();
   const { description, image, price, rating, service_name, _id } = service;
   console.log(service);
   return (
-    <div className="w-10/12 m-auto">
-      <div className="hero min-h-screen bg-base-200">
+    <div className="w-10/12 m-auto my-5">
+      <div className="hero my-5 rounded-md shadow-md bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
           <img className="rounded-lg" src={image} alt="" />
           <div>
@@ -25,6 +26,14 @@ const ServiceDetails = () => {
       </div>
 
       {/* review section */}
+      <div className="review_section w-5/12">
+            <form>
+             <div className="form_div">
+            <input type="text" placeholder=" Enter Your Review Here " />
+            <button type="summit">Submit</button>
+             </div>
+            </form>
+      </div>
     </div>
   );
 };
